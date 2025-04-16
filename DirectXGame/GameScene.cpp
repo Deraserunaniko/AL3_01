1fa88c3f4191936c66922b4604d782e73d7ceb9b
Deraserunaniko <k024g0076@g.neec.ac.jp>
2025-04-16 15:27:35 +0900

    git

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,12 +2,23 @@
 
 using namespace KamataEngine;
 
+GameScene::~GameScene() { delete model_; }
+
 void GameScene::Initialize() {
-
+	textureHandle_ = TextureManager::Load("Yoshi.jpg");
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	camera_.Initialize();
 }
-void GameScene::Update() {
 
-}
+void GameScene::Update() {}
 void GameScene::Draw() {
-
+	// DirectXCommonのインスタンスを取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	// 描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+	// 描画
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+	// 描画後処理
+	Model::PostDraw();
 }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,15 +1,28 @@
 #pragma once
 #include "KamataEngine.h"
 
-//ゲームシーン
+// ゲームシーン
 class GameScene {
+
 public:
-	//初期化
+	~GameScene();
+
+	// 初期化
 	void Initialize();
-	
-	//更新
+
+	// 更新
 	void Update();
 
-	//描画
+	// 描画
 	void Draw();
+
+private:
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// モデル
+	KamataEngine::Model* model_ = nullptr;
+	// ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+	// カメラ
+	KamataEngine::Camera camera_;
 };
