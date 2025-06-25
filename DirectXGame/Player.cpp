393e85e00872a78e7916b68a66870b82141264b8
Deraserunaniko <k024g0076@g.neec.ac.jp>
2025-06-25 16:53:16 +0900

    a

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,172 +1,129 @@
-#include "GameScene.h"
+#define NOMINMAX
+#include "Player.h"
+#include "UpData.h"
+#include "numbers"
+#include <algorithm>
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {
-	// ここにインゲームの初期化処理を書く
-	// textureHandle_ = TextureManager::Load("player.png");
+void Player::Initialize(KamataEngine::Model* model, Camera* camera, const Vector3& position) {
+	assert(model);
 
-	////スプライトインスタンスの生成
-	// sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
-	model_ = Model::Create();
-
-	blockModel_ = Model::CreateFromOBJ("block");
-
-	debugCamera_ = new DebugCamera(1280, 720);
-
-	// 自キャラの生成
-	player_ = new Player();
-
-	// 座標をマップチップ番号で指定
-	Vector3 playerPosition = mapChipField_->GetMapChipPositionByIndex(1, 18);
-
-	modelPlayer_ = Model::CreateFromOBJ("player", true);
-
-	// 自キャラの初期化
-	player_->Initialize(modelPlayer_, &camera_, playerPosition);
+	model_ = model;
+	camera_ = camera;
 
 	worldTransform_.Initialize();
 
-	// カメラの初期化
-	camera_.Initialize();
+	worldTransform_.translation_ = position;
 
-	camera_.farZ = 1000.0f;
+	// worldTransform_.translation_ = {5.0f, 5.0f, 0.0f};
 
-	// 02_03天球
-	// skydome生成
-	skydome_ = new Skydome();
-	// 初期化
-	modelSkydome_ = Model::CreateFromOBJ("skyDome", true);
-	skydome_->Initialize(modelSkydome_, &camera_);
-
-	mapChipField_ = new MapChipField;
-
-	mapChipField_->LoadMapChipCsv("Resources/blocks.csv");
-
-	GenerateBlocks();
-
-	CController_ = new CameraController(); // 生成
-
-	CController_->Initialize(&camera_); // 初期化
-
-	CController_->SetTarget(player_); // 追従対象セット
-
-	CController_->Reset(); // リセット
-
-	CameraController::Rect cameraArea = {12.0f, 100 - 12.0f, 6.0f, 6.0f};
-	CController_->SetMovableArea(cameraArea);
+	worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f;
 }
 
-void GameScene::GenerateBlocks() {
-	// 要素数
-	uint32_t numBlockVirtical = mapChipField_->GetNumBlockVirtical();
-	uint32_t numBlockHorizontal = mapChipField_->GetNumBlockHorizontal();
+void Player::UpDate() {
+	// for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
+	// 	for (WorldTransform*& worldTransformBlock : worldTransformBlockLine) {
+	// 		if (!worldTransformBlock)
+	// 			continue;
+	// 		// アフィン変換行列の生成
+	// 		worldTransformBlock->matWorld_ = MakeAffineMatrix(worldTransformBlock->scale_, worldTransformBlock->rotation_, worldTransformBlock->translation_);
+	//
+	// 		// 定数バッファに転送する
+	// 		worldTransformBlock->TransferMatrix();
+	// 	}
+	//
 
-	// 要素数を変更する
+	// 移動入力
+	// 接地状態
 
-	worldTransformBlocks_.resize(numBlockVirtical);
+	// 接地判定
+	// アフィン変換行列の生成
+	worldTransform_.matWorld_ = MakeAffineMatrix(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
 
-	// キューブの生成
-	for (uint32_t i = 0; i < numBlockVirtical; ++i) {
-		worldTransformBlocks_[i].resize(numBlockHorizontal);
-	}
-	// ブロックの生成
-	for (uint32_t i = 0; i < numBlockVirtical; ++i) {
-		for (uint32_t j = 0; j < numBlockHorizontal; ++j) {
-			if (mapChipField_->GetMapChipTypeByIndex(j, i) == MapChipType::kBlock) {
-				WorldTransform* worldTransform = new WorldTransform();
-				worldTransform->Initialize();
-				worldTransformBlocks_[i][j] = worldTransform;
-				worldTransformBlocks_[i][j]->translation_ = mapChipField_->GetMapChipPositionByIndex(j, i);
+	// 定数バッファに転送する
+	worldTransform_.TransferMatrix();
+	if (onGround_) {
+		if (Input::GetInstance()->PushKey(DIK_D) || Input::GetInstance()->PushKey(DIK_A)) {
+			Vector3 acceleration = {};
+			if (Input::GetInstance()->PushKey(DIK_D)) {
+				if (velocity_.x < 0.0f) {
+					velocity_.x *= (1.0f - kAttenuation);
+				}
+				acceleration.x += kAcceleration;
+				if (lrDirection_ != LRDirection::kRight) {
+					lrDirection_ = LRDirection::kRight;
+
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
+
+			} else if (Input::GetInstance()->PushKey(DIK_A)) {
+				if (velocity_.x > 0.0f) {
+					velocity_.x *= (1.0f - kAttenuation);
+				}
+				acceleration.x -= kAcceleration;
+				if (lrDirection_ != LRDirection::kLeft) {
+					lrDirection_ = LRDirection::kLeft;
+
+					turnFirstRotationY_ = worldTransform_.rotation_.y;
+					turnTimer_ = kTimeTurn;
+				}
 			}
+			velocity_ = Add(velocity_, acceleration);
+
+			velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
+		} else {
+			velocity_.x *= (1.0f - kAcceleration);
 		}
-	}
-}
-
-void GameScene::Update() {
-	// ここにインゲームの更新処理を書
-
-	// 自キャラの更新
-	player_->UpDate();
-	skydome_->Update();
-
-	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
-		for (WorldTransform*& worldTransformBlock : worldTransformBlockLine) {
-			if (!worldTransformBlock)
-				continue;
-			// アフィン変換行列の生成
-			worldTransformBlock->matWorld_ = MakeAffineMatrix(worldTransformBlock->scale_, worldTransformBlock->rotation_, worldTransformBlock->translation_);
-
-			// 定数バッファに転送する
-			worldTransformBlock->TransferMatrix();
+		if (Input::GetInstance()->PushKey(DIK_SPACE)) {
+			velocity_ = Add(Vector3(0, kJumpAcceleration, 0), velocity_);
 		}
-	}
-
-#ifdef _DEBUG
-	// デバックの時Cキーを押すと状態が反転する
-	if (Input::GetInstance()->TriggerKey(DIK_C)) {
-		isDebugCameraActive_ = !isDebugCameraActive_;
-	}
-#endif // ! _DEBUG
-	// カメラの処理
-	if (isDebugCameraActive_) {
-		debugCamera_->Update();
-		camera_.matView = debugCamera_->GetCamera().matView;
-		camera_.matProjection = debugCamera_->GetCamera().matProjection;
-		// ビュープロジェクション行列の転送AL3_02_02*/
-		camera_.TransferMatrix();
 	} else {
-		// ビュープロジェクション行列の更新と転送AL3_02_02*/
-
-		camera_.UpdateMatrix();
+		velocity_ = Add(Vector3(0, -kGravityAcceleration / 60, 0), velocity_);
+		velocity_.y = std::max(velocity_.y, -kLimitFallSpeed);
 	}
-	CController_->Updata();
+
+	worldTransform_.translation_ = Add(velocity_, worldTransform_.translation_);
+	upData->WorldTransformUpData(worldTransform_);
+
+	bool landing = false;
+
+	if (velocity_.y < 0) {
+		if (worldTransform_.translation_.y <= 1.0f) {
+			landing = true;
+		}
+	}
+
+	if (onGround_) {
+		if (velocity_.y > 0.0f) {
+			onGround_ = false;
+		}
+	} else {
+		if (landing) {
+			worldTransform_.translation_.y = 1.0f;
+
+			velocity_.x *= (1.0f - kAttenuation);
+
+			velocity_.y = 0.0f;
+
+			onGround_ = true;
+		}
+	}
+	if (turnTimer_ > 0.0f) {
+		turnTimer_ = std::max(turnTimer_ - (1.0f / 60.0f), 0.0f);
+		float destinationRotationYTable[] = {std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f};
+		float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrDirection_)];
+		worldTransform_.rotation_.y = EaseInOut(destinationRotationY, turnFirstRotationY_, turnTimer_ / kTimeTurn);
+	}
 }
 
-void GameScene::Draw() {
-
-	player_->Draw();
-
-	// 天球描画
-	skydome_->Draw();
-
+void Player::Draw() {
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
 	Model::PreDraw(dxCommon->GetCommandList());
 
-	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
-		for (WorldTransform*& worldTransformBlock : worldTransformBlockLine) {
-			if (!worldTransformBlock)
-				continue;
-			blockModel_->Draw(*worldTransformBlock, camera_);
-		}
-	}
-
-	// modelPlayer_->Draw(*worldTransformBlock, camera_);
+	model_->Draw(worldTransform_, *camera_);
 
 	Model::PostDraw();
-}
-
-GameScene::~GameScene() {
-	// delete sprite_;
-	delete debugCamera_;
-
-	delete model_;
-
-	delete blockModel_;
-
-	delete skydome_;
-
-	delete modelPlayer_;
-
-	delete player_;
-	for (std::vector<WorldTransform*>& worldTransformBlockLine : worldTransformBlocks_) {
-		for (WorldTransform*& worldTransformBlock : worldTransformBlockLine) {
-			delete worldTransformBlock;
-		}
-	}
-	worldTransformBlocks_.clear();
-
-	delete mapChipField_;
 }
